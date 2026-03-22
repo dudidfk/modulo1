2b6ba8cfe3ffdba72558bc88bea73e7d0985f333
dudidfk <hondinha270107@gmail.com>
2026-03-22 14:33:31 -0300

    organizador de arquivos add

---
+++ organizar-arquivos.py
@@ -0,0 +1,25 @@
+import os 
+import shutil
+
+pasta = "C:/Users/Administrator/Downloads"
+arquivos = os.listdir(pasta)
+
+for arquivo in arquivos:
+    caminho = os.path.join(pasta, arquivo)
+
+    if os.path.isfile(caminho):
+        extensao = arquivo.split(".")[-1].lower()
+
+        if extensao in ["png", "jpg", "jpeg"]:
+            destino = os.path.join(pasta, "Imagens")
+        elif extensao in ["mp4", "mkv"]:
+            destino = os.path.join(pasta, "Vídeos")
+        else:
+            destino = os.path.join(pasta, "Outros")
+        if not os.path.exists(destino):
+            os.mkdir(destino)
+        
+        novo_caminho = os.path.join(destino, arquivo)
+        shutil.move(caminho, novo_caminho)
+
+        print(f"Movendo: {arquivo} -> {destino}")
